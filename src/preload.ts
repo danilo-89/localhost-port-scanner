7934749e7452bad679247fa1ca8313b3333f3d79
danilo-89 <danilo89abcd@gmail.com>
2023-09-16 00:33:34 +0200

    added exposed functions

--- src/preload.ts
+++ src/preload.ts
@@ -1,2 +1,16 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+
+import { contextBridge, ipcRenderer } from 'electron';
+
+contextBridge.exposeInMainWorld('electronAPI', {
+	startServer: () => {
+		ipcRenderer.invoke('start-server');
+	},
+	stopServer: () => {
+		ipcRenderer.invoke('stop-server');
+	},
+	scanPort: () => {
+		ipcRenderer.invoke('scan-port');
+	},
+});
