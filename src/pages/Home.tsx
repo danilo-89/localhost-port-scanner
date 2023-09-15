50fc73981886cc5c0da87fff22d01b941cdb3758
danilo-89 <danilo89abcd@gmail.com>
2023-09-16 00:33:58 +0200

    added buttons for testing purposes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
 
 const Home = () => {
-	return <div>Home</div>;
+	return (
+		<div>
+			Home
+			<div>
+				<button
+					type='button'
+					onClick={() => {
+						window.electronAPI.startServer();
+					}}
+				>
+					Start Server
+				</button>
+				<button
+					type='button'
+					onClick={() => {
+						window.electronAPI.stopServer();
+					}}
+				>
+					Stop Server
+				</button>
+				<button
+					type='button'
+					onClick={() => {
+						window.electronAPI.scanPort();
+					}}
+				>
+					Scan Port
+				</button>
+			</div>
+		</div>
+	);
 };
 
 export default Home;
