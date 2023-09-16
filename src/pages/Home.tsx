023c524ce0e672864b1fad9e7ae25246e02aefe5
danilo-89 <danilo89abcd@gmail.com>
2023-09-17 00:35:25 +0200

    PortsToScan component included WIP

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,22 @@
 import ActivePortsWrapper from '@/components/ActivePortsWrapper';
+import PortsToScan from '@/components/PortsToScan';
 
 const Home = () => {
+	// console.log(range(1, 10));
+	// console.log(range(10, 1));
+	// port ranges: 0 - 1_024 - 65_536
+	// The range 0-1023 is reserved by TCP/IP for the "well-known ports", the ones commonly used by system and network services
+	// https://en.wikipedia.org/wiki/List_of_TCP_and_UDP_port_numbers
+
+	const scanPorts = async () => {
+		try {
+			const response = await window.electronAPI.scanPorts();
+			console.log(response);
+		} catch (error: unknown) {
+			console.log(error);
+		}
+	};
+
 	return (
 		<div>
 			Home
@@ -32,7 +48,16 @@ const Home = () => {
 				>
 					Scan Port
 				</button>
+				<button
+					type='button'
+					onClick={() => {
+						scanPorts();
+					}}
+				>
+					Scan Ports
+				</button>
 			</div>
+			<PortsToScan />
 		</div>
 	);
 };
