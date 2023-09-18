9d1cece6986349e8edef00bdaebc01e39da9af74
danilo-89 <danilo89abcd@gmail.com>
2023-09-18 21:53:18 +0200

    colors added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
 	content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
 	theme: {
 		extend: {},
+		colors: {
+			darkGunmetal: '#1D2029',
+			lotion: '#FDFDFB',
+			yankeesBlue: '#252B43',
+			charcoal: '#323853',
+		},
 	},
 	variants: {},
 	plugins: [],
