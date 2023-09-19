872941ac4a9678d7c5d48f5c274f700d501283ab
danilo-89 <danilo89abcd@gmail.com>
2023-09-19 17:23:25 +0200

    config extended

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-	theme: {
-		extend: {},
-		colors: {
-			darkGunmetal: '#1D2029',
-			lotion: '#FDFDFB',
-			yankeesBlue: '#252B43',
-			charcoal: '#323853',
-		},
-	},
-	variants: {},
-	plugins: [],
-};
+    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            boxShadow: {
+                'inner-discreet':
+                    'inset 1px 1px 0 0 rgb(255 255 255 / 0.18), inset -1px -1px 1px 0 rgb(255 255 255 / 0.15)',
+                'inner-discreet-hover':
+                    'inset 3px 3px 20px 0 rgb(255 255 255 / 0.18), inset -3px -3px 20px 0 rgb(255 255 255 / 0.15)',
+            },
+        },
+        colors: {
+            darkGunmetal: '#1D2029',
+            lotion: '#FDFDFB',
+            yankeesBlue: '#252B43',
+            charcoal: '#323853',
+        },
+    },
+    variants: {},
+    plugins: [],
+}
+
