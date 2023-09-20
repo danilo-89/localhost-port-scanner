b9d63536042668c6c9ba1f36e616c51ac2d35cfe
danilo-89 <danilo89abcd@gmail.com>
2023-09-20 14:37:16 +0200

    added missing types

--- src/types/renderer.d.ts
+++ src/types/renderer.d.ts
@@ -2,6 +2,10 @@ export interface IElectronAPI {
     startServer: () => void
     stopServer: () => void
     scanPort: () => void
+    stopScanning: () => void
+    scanPorts: (arg1: (number | number[])[]) => Promise<void>
+    getIP: () => Promise<string>
+    initPercent: (arg1: Dispatch<{ percentOfScanning: number }>) => void
 }
 
 declare global {
