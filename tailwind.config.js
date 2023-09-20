8b639cfc360a0023cc5bb7ce6efe8217659bbc42
danilo-89 <danilo89abcd@gmail.com>
2023-09-20 09:43:32 +0200

    keyframes and color added

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,12 +9,30 @@ module.exports = {
                 'inner-discreet-hover':
                     'inset 3px 3px 20px 0 rgb(255 255 255 / 0.18), inset -3px -3px 20px 0 rgb(255 255 255 / 0.15)',
             },
+						keyframes: {
+							'dot-1': {
+								'0%': { opacity: '0.1' },
+								'10%': { opacity: '0.1' },
+								'100%': { opacity: '1' },
+							},
+							'dot-2': {
+								'0%': { opacity: '0.05' },
+								'30%': { opacity: '0.1' },
+								'100%': { opacity: '1' },
+							},
+							'dot-3': {
+								'0%': { opacity: '0' },
+								'70%': { opacity: '0.1' },
+								'100%': { opacity: '1' },
+							},
+						}
         },
         colors: {
             darkGunmetal: '#1D2029',
             lotion: '#FDFDFB',
             yankeesBlue: '#252B43',
             charcoal: '#323853',
+						manatee: '#9DA3AE'
         },
     },
     variants: {},
