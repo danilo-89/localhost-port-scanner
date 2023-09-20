3f0dc48703fa62519ce145179e6b43767a68e3a1
danilo-89 <danilo89abcd@gmail.com>
2023-09-20 14:36:30 +0200

    added types

---
+++ src/types/types.ts
@@ -0,0 +1,26 @@
+export interface ScannedPortsState {
+    data: null | any
+    isLoading: boolean
+    percentOfScanning: undefined | number
+    error: null | any
+}
+
+export type ScannedPortsDispatch = (arg1: Partial<ScannedPortsState>) => void
+
+export type ScannedPortsScanPorts = (
+    arg1: (number | number[])[]
+) => Promise<void>
+
+export interface IScannedPorts {
+    state: ScannedPortsState
+    dispatch: ScannedPortsDispatch
+    scanPorts: ScannedPortsScanPorts
+}
+
+export interface ScanPortResponse {
+    port: number
+    statusMessage: string
+    statusCode: undefined | number
+    error: undefined | true
+    headers: any
+}
