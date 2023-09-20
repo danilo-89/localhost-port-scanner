36e1d1fa07d38802f60a9d51f44c26dcdbeec0f8
danilo-89 <danilo89abcd@gmail.com>
2023-09-20 09:48:31 +0200

    function extracted from useEffect

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,26 +1,30 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
+import { SvgArrowPath } from '@/components/icons';
+import Button from '../common/Button';
 
 const Footer = () => {
 	const [ipAddress, setIpAddress] = useState(undefined);
 
 	console.log(ipAddress);
 
-	useEffect(() => {
-		const provideGetIP = async () => {
-			try {
-				setIpAddress(await window.electronAPI.getIP());
-			} catch (error) {
-				setIpAddress(undefined);
-			}
-		};
-
-		provideGetIP();
+	const provideGetIP = useCallback(async () => {
+		try {
+			setIpAddress(await window.electronAPI.getIP());
+		} catch (error) {
+			setIpAddress(undefined);
+		}
 	}, []);
 
+	useEffect(() => {
+		provideGetIP();
+	}, [provideGetIP]);
+
 	return (
-		<footer className='fixed bottom-0 left-0 right-0 px-3 py-1 text-[gray] bg-[#2a2e3b] flex justify-between text-sm'>
-			<div>localhost: {ipAddress}</div>
-			<button>refresh</button>
+		<footer className='fixed bottom-0 left-0 right-0 px-3 py-1 text-[#939aaf] bg-[#2a2e3b] flex justify-between text-sm'>
+			<div>{ipAddress}</div>
+			<Button variation='transparent' size='sm' onClick={provideGetIP}>
+				<SvgArrowPath className='w-3 h-3' />
+			</Button>
 		</footer>
 	);
 };
