6a9694cd49918f015311a951c5ff4b14f98ad3f1
danilo-89 <danilo89abcd@gmail.com>
2023-09-22 12:33:25 +0200

    added port range message

--- src/components/ScannedPortInfo/ScannedPortInfo.tsx
+++ src/components/ScannedPortInfo/ScannedPortInfo.tsx
@@ -5,6 +5,8 @@ import { scannedPortInfoConfig } from './scannedPortInfoConfig'
 
 // Utilities
 import { getStatusMessageInfo } from '@/utils/getStatusMessageInfo'
+import { getPortRangeMessage } from '@/utils/getPortRangeMessage'
+import { PORT_RANGE_MESSAGES_SOURCE } from '@/constants/portRangeMessages'
 
 // TS
 interface IProps {
@@ -15,14 +17,28 @@ const ScannedPortInfo = ({ clickedRowInfo }: IProps) => {
     return (
         <div className="mx-auto w-[35rem] bg-yankeesBlue">
             <div className="mb-4 bg-darkGunmetal p-4">
-                <h2 className="text-lg font-bold">
+                <h2 className="mb-2 text-lg font-bold">
                     <span className="rounded-md bg-charcoal px-2">
                         {clickedRowInfo.port}
                     </span>{' '}
                     Port Info
                 </h2>
+                <div className="text-sm text-manatee">
+                    {getPortRangeMessage(clickedRowInfo.port)} [{' '}
+                    <a
+                        href={PORT_RANGE_MESSAGES_SOURCE}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="mt-1 text-sm text-[#619ad6] underline"
+                    >
+                        list of TCP and UDP port numbers
+                    </a>{' '}
+                    ]
+                </div>
+                <div></div>
             </div>
             <div className="p-5">
+                {/* <span className="mb-8 block border-b border-b-charcoal pt-2" /> */}
                 <ul className="flex flex-col">
                     {scannedPortInfoConfig.map((item) => {
                         const itemValue = clickedRowInfo[item.key]
