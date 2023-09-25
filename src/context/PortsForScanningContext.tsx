fffd3a2a077e8374a3c22f256c2ea1166992b1a3
danilo-89 <danilo89abcd@gmail.com>
2023-09-25 10:07:20 +0200

    refactoring

--- src/context/PortsForScanningContext.tsx
+++ src/context/PortsForScanningContext.tsx
@@ -1,20 +1,46 @@
-import { createContext, ReactNode, useContext, useReducer } from 'react'
+import {
+    createContext,
+    ReactNode,
+    useContext,
+    useEffect,
+    useReducer,
+    useState,
+} from 'react'
+import range from 'lodash.range'
+import sortedUniq from 'lodash.sorteduniq'
 
 type StateItem = number | [number, number]
 
 interface IPortsForScanningContext {
     state: StateItem[]
-    dispatch: (arg1: StateItem) => void
+    dispatch: (arg1: { name: 'add' | 'remove'; payload: StateItem }) => void
+    portsForScanning: number[]
 }
 
 const PortsForScanningContext = createContext<IPortsForScanningContext>({
     state: [],
     dispatch: (_arg1) => null,
+    portsForScanning: [],
 })
 
-const reducer = (state: StateItem[], action: StateItem) => {
-    if (action !== undefined) {
-        return [...state, action]
+const reducer = (
+    state: StateItem[],
+    action: { name: 'add' | 'remove'; payload: StateItem }
+) => {
+    if (action.name === 'add') {
+        if (
+            typeof action.payload === 'number' &&
+            state.includes(action.payload)
+        ) {
+            console.log('port already added')
+            return state
+        }
+        return [...state, action.payload]
+    }
+    if (action.name === 'remove' && typeof action.payload === 'number') {
+        const copyOfState = [...state]
+        copyOfState.splice(action.payload, 1)
+        return copyOfState
     }
     return state
 }
@@ -25,16 +51,41 @@ export function PortsForScanningProvider({
     children: ReactNode
 }) {
     const [state, dispatch] = useReducer(reducer, [
-        [0, 6001],
+        [3_000, 11_000],
+        // [50_000, 65_535],
         9999999999999,
         7000,
+        3001,
+        // [1000, 3000],
     ])
+
+    const [portsForScanning, setPortsForScanning] = useState([])
+
+    useEffect(() => {
+        const parsedPorts = sortedUniq(
+            state
+                .reduce((acc, curr) => {
+                    if (
+                        Array.isArray(curr) &&
+                        typeof curr[0] === 'number' &&
+                        typeof curr[1] === 'number'
+                    ) {
+                        return [...acc, ...range(curr[0], curr[1] + 1)]
+                    }
+                    return [...acc, curr]
+                }, [])
+                .sort((a, b) => a - b)
+        )
+
+        setPortsForScanning(() => parsedPorts)
+    }, [state])
 
     return (
         <PortsForScanningContext.Provider
             value={{
                 state,
                 dispatch,
+                portsForScanning,
             }}
         >
             {children}
