c3574ea38f04300fd77c9a2c06a104eadc9e28e5
danilo-89 <danilo89abcd@gmail.com>
2023-09-25 10:06:49 +0200

    label change

--- src/components/ScannedPortInfo/scannedPortInfoConfig.ts
+++ src/components/ScannedPortInfo/scannedPortInfoConfig.ts
@@ -2,6 +2,6 @@ export const scannedPortInfoConfig = [
     // { label: 'Port', key: 'port', elementType: 'code' },
     { label: 'Status Message', key: 'statusMessage', elementType: 'code' },
     { label: 'Status Code', key: 'statusCode', elementType: 'code' },
-    { label: 'Error', key: 'error', elementType: 'code' },
+    { label: 'Response Error', key: 'error', elementType: 'code' },
     { label: 'Response headers', key: 'headers', elementType: 'pre' },
 ]
