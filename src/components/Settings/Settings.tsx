0c326a8a9dca9ec787a3cee33064aaf8e8f07d18
danilo-89 <danilo89abcd@gmail.com>
2023-09-25 09:54:23 +0200

    refactoring

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -5,14 +5,12 @@ import range from 'lodash.range'
 import { useEffect, useReducer, useState } from 'react'
 import Button from '../common/Button'
 
-// sortedUniqBy
-
 const Settings = () => {
     // const [state, dispatch] = useReducer(reducer, [[3000, 9001], 2000]);
     const { state, dispatch } = usePortsForScanningContext()
-    const [inputLeft, setInputLeft] = useState(undefined)
-    const [inputRight, setInputRight] = useState(undefined)
-    const [inputSingle, setInputSingle] = useState(undefined)
+    const [inputLeft, setInputLeft] = useState<'' | number>('')
+    const [inputRight, setInputRight] = useState<'' | number>('')
+    const [inputSingle, setInputSingle] = useState<'' | number>('')
     const [showRange, setShowRange] = useState(false)
     // const [percent, setPercent] = useState(0);
     const { state: scanningResult, scanPorts } = useScannedPortsContext()
@@ -20,10 +18,15 @@ const Settings = () => {
     console.log(state)
 
     const addToState = () => {
-        if (showRange) {
-            dispatch([inputLeft, inputRight])
-        } else {
-            dispatch(inputSingle)
+        if (
+            showRange &&
+            typeof inputLeft === 'number' &&
+            typeof inputRight === 'number'
+        ) {
+            dispatch({ name: 'add', payload: [inputLeft, inputRight] })
+        }
+        if (!showRange && typeof inputSingle === 'number') {
+            dispatch({ name: 'add', payload: inputSingle })
         }
     }
 
@@ -40,95 +43,136 @@ const Settings = () => {
     console.log({ CHECK: state })
 
     return (
-        <div className="w-[30rem] bg-yankeesBlue p-5">
-            <div className="mb-8">
-                <h2 className="text-lg font-bold">Settings</h2>{' '}
+        <div className="w-[30rem] bg-yankeesBlue">
+            <div className="mb-4 bg-darkGunmetal p-4">
+                <h2 className="text-lg font-bold">Settings</h2>
             </div>
-            <div className="styled-scrollbar flex h-[7.5rem] flex-wrap content-start justify-start gap-1 overflow-auto rounded-lg bg-charcoal p-3">
-                {state.map((item: number | number[]) => {
-                    const itemValue = Array.isArray(item)
-                        ? item.join(' - ')
-                        : item
+            <div className="p-5">
+                <span>Current ports for scanning:</span>
+                <div className="styled-scrollbar mb-4 flex h-[7.5rem] flex-wrap content-start justify-start gap-1 overflow-auto rounded-lg bg-charcoal p-2">
+                    {state.map((item: number | [number, number], idx) => {
+                        const itemValue = Array.isArray(item)
+                            ? item.join(' - ')
+                            : item
 
-                    return (
-                        <span
-                            className="inline-block rounded-full bg-darkGunmetal px-2 py-1 text-sm"
-                            key={itemValue}
-                        >
-                            {itemValue}{' '}
-                            <Button
-                                type="button"
-                                variation="neutral"
-                                size="sm"
-                                className="rounded-full"
-                                onClick={() => console.log(item)}
+                        return (
+                            <span
+                                className="inline-block rounded-full bg-darkGunmetal px-2 py-1 text-sm"
+                                key={itemValue}
                             >
-                                x
-                            </Button>
-                        </span>
-                    )
-                })}
-            </div>
-            <form
-                onSubmit={(e) => {
-                    e.preventDefault()
-                    addToState()
-                }}
-            >
-                {showRange ? (
-                    <div className="flex">
-                        <input
-                            key="inputLeft"
-                            value={inputLeft}
-                            onChange={(e) => setInputLeft(+e.target.value)}
-                            className="w-[40%] border"
-                            type="number"
-                            name=""
-                            id=""
-                        />{' '}
-                        -{' '}
-                        <input
-                            key="inputRight"
-                            value={inputRight}
-                            onChange={(e) => setInputRight(+e.target.value)}
-                            className="w-[40%] border"
-                            type="number"
-                            name=""
-                            id=""
-                        />
-                    </div>
-                ) : (
-                    <div>
-                        <input
-                            key="inputSingle"
-                            value={inputSingle}
-                            onChange={(e) => setInputSingle(+e.target.value)}
-                            className="border"
-                            type="number"
-                            name=""
-                            id=""
-                        />
-                    </div>
-                )}
-                <label htmlFor="rangeCheckbox">range</label>
-                <input
-                    type="checkbox"
-                    name="range"
-                    id="rangeCheckbox"
-                    checked={showRange}
-                    onChange={() => setShowRange((curr) => !curr)}
-                />
-                <br />
-                <Button
-                    type="submit"
-                    variation="tertiary"
-                    size="md"
-                >
-                    Add Ports
-                </Button>
-            </form>
+                                {itemValue}{' '}
+                                <Button
+                                    type="button"
+                                    variation="neutral"
+                                    size="sm"
+                                    className="rounded-full"
+                                    onClick={() =>
+                                        dispatch({
+                                            name: 'remove',
+                                            payload: idx,
+                                        })
+                                    }
+                                >
+                                    x
+                                </Button>
+                            </span>
+                        )
+                    })}
+                </div>
 
-            <br />
+                <span>Add ports for scanning:</span>
+                <form
+                    onSubmit={(e) => {
+                        e.preventDefault()
+                        addToState()
+                    }}
+                    className="rounded-lg border-2 border-charcoal p-4 "
+                >
+                    <div className="mb-4 flex items-center">
+                        {showRange ? (
+                            <div className="flex items-center">
+                                <input
+                                    key="inputLeft"
+                                    step={1}
+                                    min={0}
+                                    max={65_535}
+                                    value={inputLeft}
+                                    onChange={(e) =>
+                                        setInputLeft(+e.target.value)
+                                    }
+                                    className="border py-1.5 pl-4 pr-1"
+                                    type="number"
+                                    name="inputLeft"
+                                    id="inputLeft"
+                                />
+                                <span className="mx-3 inline-block">-</span>
+                                <input
+                                    key="inputRight"
+                                    step={1}
+                                    min={0}
+                                    max={65_535}
+                                    value={inputRight}
+                                    onChange={(e) =>
+                                        setInputRight(+e.target.value)
+                                    }
+                                    className="border py-1.5 pl-4 pr-1"
+                                    type="number"
+                                    name="inputRight"
+                                    id="inputRight"
+                                />
+                            </div>
+                        ) : (
+                            <div>
+                                <input
+                                    key="inputSingle"
+                                    step={1}
+                                    min={0}
+                                    max={65_535}
+                                    value={inputSingle}
+                                    onChange={(e) =>
+                                        setInputSingle(+e.target.value)
+                                    }
+                                    className="border py-1.5 pl-4 pr-1"
+                                    type="number"
+                                    name="inputSingle"
+                                    id="inputSingle"
+                                    placeholder="port number"
+                                />
+                            </div>
+                        )}
+                        <div className="mb-2 ml-auto flex items-center">
+                            {' '}
+                            <input
+                                type="checkbox"
+                                name="range"
+                                id="rangeCheckbox"
+                                checked={showRange}
+                                onChange={() => setShowRange((curr) => !curr)}
+                                className="mr-1"
+                            />
+                            <label htmlFor="rangeCheckbox">range</label>
+                        </div>
+                    </div>
+
+                    <div className="text-center">
+                        {' '}
+                        <Button
+                            type="submit"
+                            variation="tertiary"
+                            size="md"
+                            disabled={
+                                (showRange &&
+                                    (typeof inputLeft !== 'number' ||
+                                        typeof inputRight !== 'number')) ||
+                                (!showRange && typeof inputSingle !== 'number')
+                            }
+                        >
+                            Add Ports
+                        </Button>
+                    </div>
+                </form>
+                <br />
+            </div>
         </div>
     )
 }
