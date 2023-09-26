d26c5217869d00b09735665daa2e286adfe0fd72
danilo-89 <danilo89abcd@gmail.com>
2023-09-26 22:48:42 +0200

    added close button

--- src/components/common/Modal/Modal.tsx
+++ src/components/common/Modal/Modal.tsx
@@ -1,6 +1,9 @@
 import { Dispatch, ReactNode, SetStateAction } from 'react'
 import * as Dialog from '@radix-ui/react-dialog'
 
+// Components
+import Button from '@/components/common/Button'
+
 interface IProps {
     children: ReactNode
     preventOverlayClose?: boolean
@@ -17,11 +20,20 @@ const Modal = ({ children, preventOverlayClose, setIsOpen }: IProps) => {
             <Dialog.Portal>
                 <Dialog.Overlay className="fixed inset-0 z-10 flex items-center justify-center bg-[rgba(0,0,0,0.80)]">
                     <Dialog.Content
-                        className="bg-white mx-auto max-h-full max-w-full overflow-auto rounded"
+                        className="bg-white relative mx-auto max-h-full max-w-full overflow-auto rounded"
                         onInteractOutside={(e) => {
                             if (preventOverlayClose) e.preventDefault()
                         }}
                     >
+                        <Button
+                            variation="transparent"
+                            hasBorder={false}
+                            size="sm"
+                            className="absolute right-2 top-2.5 h-10 w-10"
+                            onClick={() => setIsOpen(false)}
+                        >
+                            x
+                        </Button>
                         {children}
                     </Dialog.Content>
                 </Dialog.Overlay>
